5377bc59a67c664e074876bb141e43b4d3959588
nam nguyen <nguyenvuhainamhan@gmail.com>
2025-04-06 19:06:17 -0700

    Add OSM amenity category mapping to vancouver_amenities.py
    - Removed previous code for printing unique amenities.
    - Introduced a dictionary to categorize amenities into specific groups: food & drink, transportation, entertainments & culture, health & emergency, shop & services, and others.

--- vancouver_amenities.py
+++ vancouver_amenities.py
@@ -5,6 +5,25 @@ df = pd.read_json('data/amenities-vancouver.json.gz', compression='gzip', lines=
 
 df.to_csv('data/vancouver_amenities.csv', index=False)
 
-# get the type of amenities
-amenities = df['amenity'].unique()
-print(amenities)
+# filter out the neccessary amenities columns
+# OSM amenity category mapping for the project
+amenity_categories = {
+  "food & drink": [
+    'cafe', 'fast_food', 'bbq', 'restaurant', 'pub', 'bar', 'food_court', 'ice_cream', 'bistro', 'juice_bar', 'internet_cafe', 'disused:restaurant', 'water_point', 'biergarten'
+  ],
+  "transportation": [
+    'fuel', 'parking_entrance', 'bicycle_parking', 'parking', 'ferry_terminal', 'car_rental', 'car_sharing', 'bicycle_rental', 'seaplane terminal', 'charging_station', 'parking_space', 'taxi', 'bus_station', 'motorcycle_parking', 'boat_rental', 'EVSE', 'motorcycle_rental'
+  ],
+  "entertainments & culture": [
+    'place_of_worship', 'cinema', 'theatre', 'library', 'arts_centre', 'fountain', 'photo_booth', 'nightclub', 'clock', 'stripclub', 'gambling', 'playground', 'meditation_centre', 'spa', 'lounge', 'gym', 'park', 'casino', 'leisure'
+  ],
+  "health & emergency": [
+    'pharmacy', 'dentist', 'doctors', 'clinic', 'veterinary', 'hospital', 'first_aid', 'healthcare', 'chiropractor', 'Pharmacy', 
+  ],
+  "shop & services": [
+    'post_office', 'atm', 'childcare', 'bank', 'car_wash', 'luggage_locker', 'bureau_de_change', 'marketplace', 'atm;bank', 'shop|clothes'
+  ],
+  "others": [
+    'toilets', 'post_box', 'telephone', 'vending_machine', 'school', 'bench', 'community_centre', 'waste_basket', 'public_building', 'drinking_water', 'shelter', 'recycling', 'public_bookcase', 'university', 'dojo', 'bicycle_repair_station', 'waste_disposal', 'social_facility', 'college', 'construction', 'post_depot', 'nursery', 'kindergarten', 'conference_centre', 'shower', 'trolley_bay', 'fire_station', 'police', 'compressed_air', 'family_centre', 'townhall', 'music_school', 'scrapyard', 'language_school', 'courthouse', 'events_venue', 'prep_school', 'cram_school', 'science', 'ATLAS_clean_room', 'workshop', 'safety', 'lobby', 'animal_shelter', 'social_centre', 'vacuum_cleaner', 'smoking_area', 'studio', 'ranger_station', 'storage_rental', 'watering_place', 'trash', 'sanitary_dump_station', 'Observation Platform', 'housing co-op', 'driving_school', 'loading_dock', 'monastery', 'storage', 'payment_terminal', 'waste_transfer_station', 'office|financial', 'hunting_stand', 'money_transfer', 'letter_box', 'training', 'car_rep', 'research_institute'
+  ],
+}
